5e54e22d7077b3f35bfa1c89f9d56ddf7bf7bc69
fanzide <fanzide@richinfo.cn>
2019-03-11 17:42:54 +0800

    Initial commit

---
+++ src/main/java/com/github/fanzide/familyaccountbook/FamilyAccountBookApplication.java
@@ -0,0 +1,13 @@
+package com.github.fanzide.familyaccountbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyAccountBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyAccountBookApplication.class, args);
+	}
+
+}
